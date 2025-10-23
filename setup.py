475a323231d692cb4e35c13ecba3f7136841e57f
Matheus Reis <mreis.oceanscan@gmail.com>
2025-10-23 18:11:06 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,34 @@
+import os
+from glob import glob
+from setuptools import find_packages, setup
+
+package_name = 'lauv_3d_sim'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*')),
+        (os.path.join('share', package_name), glob('urdf/*')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='mreis',
+    maintainer_email='mreis@oceanscan-mst.com',
+    description='TODO: Package description',
+    license='Apache-2.0',
+    extras_require={
+        'test': [
+        'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'state_publisher = lauv_3d_sim.state_publisher:main'
+        ],
+    },
+)
