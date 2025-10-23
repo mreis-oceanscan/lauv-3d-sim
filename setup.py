37675e41670cd018cb6f2bfde3a6396ad337e367
Matheus Reis <mreis.oceanscan@gmail.com>
2025-10-23 23:19:54 +0100

    Add gitignore

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ setup(
         ('share/' + package_name, ['package.xml']),
         (os.path.join('share', package_name, 'launch'), glob('launch/*')),
         (os.path.join('share', package_name), glob('urdf/*')),
+        # (os.path.join('share', package_name), glob('config/*')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
